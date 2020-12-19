129c6cdfede86ebb49fe345c103fcc8e23b4f2da
Your Name <you@example.com>
2020-12-19 11:28:00 +0800

    add

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -2,14 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use LINE\LINEBot;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\JoinEvent;
+use LINE\LINEBot\Event\LeaveEvent;
+use LINE\LINEBot\Event\MemberJoinEvent;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use Illuminate\Http\Request;
 
 class BotController extends Controller
 {
+    public function __construct()
+    {
+        $this->lineAccessToken = env('LINE_BOT_CHANNEL_ACCESS_TOKEN');
+        $this->lineChannelSecret = env('LINE_BOT_CHANNEL_SECRET');
+
+        $httpClient = new CurlHTTPClient($this->lineAccessToken);
+        $this->lineBot = new LINEBot($httpClient, ['channelSecret' => $this->lineChannelSecret]);
+    }
+
     public function reply()
     {
-        return response()->json([
-            'status' => 'success'
-        ]);
+        $signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
+
+        if ($signature == '') {
+            return;
+        }
+
+        if (!SignatureValidator::validateSignature($request->getContent(), $this->lineChannelSecret, $signature)) {
+            return;
+        }
+
+        try {
+            $events = $this->lineBot->parseEventRequest($request->getContent(), $signature);
+
+            foreach ($events as $event) {
+                $this->userId = $event->getUserId();
+                $replyToken = $event->getReplyToken();
+                Log::info(json_encode($event));
+
+                //訊息的話
+                if ($event instanceof MessageEvent) {
+                    $msgId = $event->getMessageId();
+                    $msgType = $event->getMessageType();
+
+                    //文字
+                    if ($messageType == 'text') {
+
+                    }
+                }
+
+                if ($event instanceof JoinEvent) {
+
+                }
+
+                if ($event instanceof LeaveEvent) {
+                    
+                }
+
+                if ($event instanceof MemberJoinEvent) {
+
+                }
+            }
+        } catch (Exception $e) {
+            Log::error($e);
+            return;
+        }
+        return;
     }
 }
